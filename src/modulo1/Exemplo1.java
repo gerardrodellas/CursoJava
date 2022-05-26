f7d688bf6bd2586b571768d53e960799c0c1ac12
e006677 <gerard.rodellas@allianz.es>
2022-05-26 15:17:01 +0200

    First Commit

---
+++ src/modulo1/Exemplo1.java
@@ -0,0 +1,14 @@
+package modulo1;
+
+public class Exemplo1 {
+	public static void main(String[] args) {
+		//esta es mi primer linea de codigo en java
+		//sysout
+		System.out.println("Hola Gabrielito");
+        System.out.println("\n\n\nnombre\t\tapellido\tlink git"); 
+        System.out.println("\nGabriel\t\tCasas\t\thttps://gitlba/gcasas1972/cursojava_mayo2022");
+		System.out.println("para las comillas \" ");
+		System.out.println("para las barras \\ ");
+	}
+
+}
