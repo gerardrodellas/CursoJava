5bb4209adf4cd38b63c7c4fd6ff5ccd10630125e
e006677 <gerard.rodellas@allianz.es>
2022-06-01 08:07:23 +0200

    First Commit

---
+++ src/modulo2/Ejercicio4.java
@@ -0,0 +1,16 @@
+package modulo2;
+
+public class Ejercicio4 {
+	public static void main(String[] args) {
+		byte  b = 10;
+		short s = 20;
+		int   i = 30;
+		long  l = 40;
+		
+		int sumabb=b+b;
+		int sumabs=b+s;
+		int sumabi=b+i;
+		int sumaii=i+i;
+		long sumasl=s+l;
+	}
+}
